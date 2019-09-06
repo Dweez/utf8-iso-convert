346a331472a72b3f026ecb233f7144f4d5bd2d3d
dweez <zedwee@gmail.com>
2019-09-06 14:28:49 +0200

    add prettier config

--- src/components/converter.js
+++ src/components/converter.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 
-import useConverter from './useConverter'
-import './converter.css'
+import useConverter from "./useConverter"
+import "./converter.css"
 
-const utfShownAsIso = "Portez ce vieux whisky au juge blond qui fume sur son Ã®le intÃ©rieure, Ã  cÃ´tÃ© de l'alcÃ´ve ovoÃ¯de, oÃ¹ les bÃ»ches se consument dans l'Ã¢tre, ce qui lui permet de penser Ã  la cÃ¦nogÃ©nÃ¨se de l'Ãªtre dont il est question dans la cause ambiguÃ« entendue Ã  MoÃ¿, dans un capharnaÃ¼m qui, pense-t-il, diminue Ã§Ã  et lÃ  la qualitÃ© de son Åuvre."
+const utfShownAsIso = `Portez ce vieux whisky au juge blond qui fume sur son Ã®le intÃ©rieure, Ã  cÃ´tÃ© de l'alcÃ´ve ovoÃ¯de, oÃ¹ les bÃ»ches se consument dans l'Ã¢tre, ce qui lui permet de penser Ã  la cÃ¦nogÃ©nÃ¨se de l'Ãªtre dont il est question dans la cause ambiguÃ« entendue Ã  MoÃ¿, dans un capharnaÃ¼m qui, pense-t-il, diminue Ã§Ã  et lÃ  la qualitÃ© de son Åuvre.`
 
 function Converter() {
-  const [ input, setInput ] = useState(utfShownAsIso)
+  const [input, setInput] = useState(utfShownAsIso)
 
   const converted = useConverter(input)
 
   return (
-    <div className="converter">
-      <textarea className="textarea" value={input} onChange={event => setInput(event.target.value)} />
-      <div className="result">{converted}</div>
-    </div>
-  );
+    <>
+      <div className="converter">
+        <textarea
+          className="textarea"
+          value={input}
+          onChange={event => setInput(event.target.value)}
+        />
+        <div className="result">{converted}</div>
+      </div>
+      <div>
+        <button
+          onClick={event => setInput(utfShownAsIso)}
+          disabled={input === utfShownAsIso}
+        >
+          Texte par défaut
+        </button>
+        <button onClick={event => setInput("")}>Clear</button>
+      </div>
+    </>
+  )
 }
 
-export default Converter;
+export default Converter
--- src/components/useConverter.js
+++ src/components/useConverter.js
@@ -1,21 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
 
 const useConverter = input => {
-  const [ converted, setConverted ] = useState(input)
+  const [converted, setConverted] = useState(input)
 
-  useEffect( () => {
+  useEffect(() => {
     // voir: https://stackoverflow.com/a/5396742
-    if(input) {
-      try{
+    if (input) {
+      try {
         // Si la string est bien de l'UTF-8 il n'y aura pas d'erreur
-        setConverted( decodeURIComponent(escape(input)) )
-      }catch(e){
+        setConverted(decodeURIComponent(escape(input)))
+      } catch (e) {
         // Si ce n'est pas le cas, une erreur sera levée et nous pouvons estimer
         // qu'il s'agit d'une ISO string
-        setConverted( input )
+        setConverted(input)
       }
-    }else{
-      setConverted( "saisissez votre chaîne de caractère" )
+    } else {
+      setConverted("Saisissez votre texte (mal encodé) dans le champ ci-contre")
     }
   }, [input])
 
