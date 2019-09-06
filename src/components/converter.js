028998e8edfdd1c1d24cea83171e08409b54f866
dweez <zedwee@gmail.com>
2019-09-06 19:44:00 +0200

    switch iso / utf; mixed state

--- src/components/converter.js
+++ src/components/converter.js
@@ -1,33 +1,52 @@
 import React, { useState } from "react"
 
 import useConverter from "./useConverter"
+
 import "./converter.css"
 
-const utfShownAsIso = `Portez ce vieux whisky au juge blond qui fume sur son Ã®le intÃ©rieure, Ã  cÃ´tÃ© de l'alcÃ´ve ovoÃ¯de, oÃ¹ les bÃ»ches se consument dans l'Ã¢tre, ce qui lui permet de penser Ã  la cÃ¦nogÃ©nÃ¨se de l'Ãªtre dont il est question dans la cause ambiguÃ« entendue Ã  MoÃ¿, dans un capharnaÃ¼m qui, pense-t-il, diminue Ã§Ã  et lÃ  la qualitÃ© de son Åuvre.`
+const target = {
+  utf: `Portez ce vieux whisky au juge blond qui fume sur son Ã®le intÃ©rieure, Ã  cÃ´tÃ© de l'alcÃ´ve ovoÃ¯de, oÃ¹ les bÃ»ches se consument dans l'Ã¢tre, ce qui lui permet de penser Ã  la cÃ¦nogÃ©nÃ¨se de l'Ãªtre dont il est question dans la cause ambiguÃ« entendue Ã  MoÃ¿, dans un capharnaÃ¼m qui, pense-t-il, diminue Ã§Ã  et lÃ  la qualitÃ© de son Åuvre.`,
+  iso: `Portez ce vieux whisky au juge blond qui fume sur son île intérieure, à côté de l'alcôve ovoïde, où les bûches se consument dans l'âtre, ce qui lui permet de penser à la cænogénèse de l'être dont il est question dans la cause ambiguë entendue à Moÿ, dans un capharnaüm qui, pense-t-il, diminue çà et là la qualité de son œuvre.`
+}
 
 function Converter() {
-  const [input, setInput] = useState(utfShownAsIso)
-
-  const converted = useConverter(input)
-
+  const [input, setInput] = useState(["utf", target.utf])
+  const { converted, mixed } = useConverter(input)
   return (
     <>
+      <div>
+        <button
+          onClick={event => setInput(["utf", converted])}
+          // disabled={(input[0] === "utf" || mixed)}
+          disabled={input[0] === "utf"}
+        >
+          UTF-8 >> ISO-8859-1
+        </button>
+        <button
+          onClick={event => setInput(["iso", converted])}
+          // disabled={(input[0] === "iso" || mixed)}
+          disabled={input[0] === "iso"}
+        >
+          ISO-8859-1 >> UTF-8
+        </button>
+        {mixed ? "mixed !" : ""}
+      </div>
       <div className="converter">
         <textarea
           className="textarea"
-          value={input}
-          onChange={event => setInput(event.target.value)}
+          value={input[1]}
+          onChange={event => setInput([input[0], event.target.value])}
         />
         <div className="result">{converted}</div>
       </div>
       <div>
         <button
-          onClick={event => setInput(utfShownAsIso)}
-          disabled={input === utfShownAsIso}
+          onClick={event => setInput([input[0], target[input[0]]])}
+          disabled={input[1] === target[input[0]]}
         >
           Texte par défaut
         </button>
-        <button onClick={event => setInput("")}>Clear</button>
+        <button onClick={event => setInput([input[0], ""])}>Clear</button>
       </div>
     </>
   )
--- src/components/useConverter.js
+++ src/components/useConverter.js
@@ -1,25 +1,45 @@
 import { useState, useEffect } from "react"
 
 const useConverter = input => {
-  const [converted, setConverted] = useState(input)
+  const [converted, setConverted] = useState()
+  const [mixed, setMixed] = useState(false)
 
   useEffect(() => {
     // voir: https://stackoverflow.com/a/5396742
-    if (input) {
-      try {
-        // Si la string est bien de l'UTF-8 il n'y aura pas d'erreur
-        setConverted(decodeURIComponent(escape(input)))
-      } catch (e) {
-        // Si ce n'est pas le cas, une erreur sera levée et nous pouvons estimer
-        // qu'il s'agit d'une ISO string
-        setConverted(input)
+    async function convert() {
+      let res, mix
+      if (input && input.length && input[1]) {
+        if (input[0] === "utf") {
+          try {
+            // Si la string est bien de l'UTF-8 il n'y aura pas d'erreur
+            res = await decodeURIComponent(escape(input[1]))
+            mix = false
+          } catch (e) {
+            // Si ce n'est pas le cas, une erreur sera levée et nous pouvons estimer
+            // qu'il s'agit d'une ISO string
+            res = input[1]
+            mix = true
+          }
+        } else if (input[0] === "iso") {
+          // traitement pour ISO
+          try {
+            res = await unescape(encodeURIComponent(input[1]))
+            mix = false
+          } catch (e) {
+            res = input[1]
+            mix = true
+          }
+        }
+      } else {
+        res = ""
       }
-    } else {
-      setConverted("Saisissez votre texte (mal encodé) dans le champ ci-contre")
+      setMixed(mix)
+      setConverted(res)
     }
+    convert()
   }, [input])
 
-  return converted
+  return { converted, mixed }
 }
 
 export default useConverter
