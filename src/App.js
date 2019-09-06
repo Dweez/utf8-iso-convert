681f483d58a5b5ce2fc2501cc3ce998f67329271
dweez <zedwee@gmail.com>
2019-09-06 20:32:38 +0200

    remove leading text

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
     <div className="App">
       <div className="container">
         <h2>Conversion ISO-8859-1 ← → UTF-8</h2>
-        <p>Vous venez d'ouvrir un fichier et vous découvrez avec stupeur des caractères étranges (ex: Ã©, Ã®) à la place des caractères accentués ?<br/>
-        Ce petit convertisseur pourrait vous sauver la mise ! Collez votre texte dans le champs de gauche et récupérez le résultat à droite.</p>
         <Converter />
       </div>
     </div>
